3f2d5a879fbaaa21bdc3012a7795bdcb91a368e7
James Zhang <liulfbaby@gmail.com>
2024-05-14 01:19:48 +0800

    rewrite ExistsInDatabaseValidator

--- src/main/java/com/muhuang/salecrawler/shop/ExistsInDatabaseValidator.java
+++ src/main/java/com/muhuang/salecrawler/shop/ExistsInDatabaseValidator.java
@@ -1,17 +1,24 @@
 package com.muhuang.salecrawler.shop;
 
-import jakarta.annotation.Resource;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.List;
+
 public class ExistsInDatabaseValidator implements ConstraintValidator<ExistsInDatabase, String> {
 
-    @Resource
-    private ShopRepository shopRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
-        Shop byOutShopId = shopRepository.findByOutShopId(value);
-        return byOutShopId != null;
+        List<Shop> existingShop = entityManager
+                .createQuery("SELECT s FROM Shop s WHERE s.outShopId = :outShopId", Shop.class)
+                .setParameter("outShopId", value)
+                .getResultList();
+
+        return existingShop.size() == 1;
     }
 }
