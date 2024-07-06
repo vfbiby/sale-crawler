9f1f941f75ffbf7fc9c5229d6beaa678058642f7
安栋 <andong@xiaomalixing.com>
2024-07-06 12:48:59 +0800

    remove test sql script

--- src/test/resources/database/test-cate-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO cate (id, out_cate_id, cate_name, parent_id, out_shop_id)
-VALUES (1, '105703949', '06/25福利回馈', null, '105703949');
--- src/test/resources/database/test-shop-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO shop (id, out_shop_id, shop_name, shop_url)
-VALUES (1, '105703949', 'SKY', 'https://sky.taobao.com');
