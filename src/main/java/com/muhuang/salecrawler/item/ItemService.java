53ac82c87ec7379e4a90a7919d611ba73e11aafc
安栋 <andong@xiaomalixing.com>
2024-07-06 12:48:39 +0800

    fix code conflict

--- src/main/java/com/muhuang/salecrawler/item/ItemService.java
+++ src/main/java/com/muhuang/salecrawler/item/ItemService.java
@@ -1,10 +1,7 @@
 package com.muhuang.salecrawler.item;
 
-import com.muhuang.salecrawler.sale.SaleService;
 import com.muhuang.salecrawler.shop.Shop;
 import com.muhuang.salecrawler.shop.ShopRepository;
-import com.muhuang.salecrawler.taobao.TaobaoHttpClient;
-import com.muhuang.salecrawler.taobao.TaobaoSaleMonthlyResult;
 import jakarta.annotation.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,18 +15,11 @@ import java.util.stream.Collectors;
 @Service
 public class ItemService {
 
-    public static final String TOKEN = "rqPZ2yJQgNp1SQuR";
     @Resource
     private ItemRepository itemRepository;
 
     @Resource
     private ShopRepository shopRepository;
-
-    @Resource
-    private TaobaoHttpClient taobaoHttpClient;
-
-    @Resource
-    private SaleService saleService;
 
     public void save(Item item) {
         Shop inDB = shopRepository.findByOutShopId(item.getShop().getOutShopId());
@@ -53,16 +43,4 @@ public class ItemService {
         return itemRepository.findAll(pageRequest);
     }
 
-    /**
-     * 收藏
-     *
-     * @param itemId 商品id
-     */
-    public void favorite(Long itemId) {
-        Item item = itemRepository.getReferenceById(itemId);
-        TaobaoSaleMonthlyResult saleMonthlyResult = taobaoHttpClient.getMonthlySaleNum(item.getOutItemId(), TOKEN);
-        if (saleMonthlyResult.saleMonthlyNum() > 0) {
-            saleService.save(saleMonthlyResult.saleMonthlyNum(), item);
-        }
-    }
 }
