6bbbd1e65629519da942ed94e382235e932c4dc6
James Zhang <liulfbaby@gmail.com>
2024-07-17 20:16:51 +0800

    to be fix

--- src/test/java/com/muhuang/salecrawler/item/ItemSellCountTest.java
+++ src/test/java/com/muhuang/salecrawler/item/ItemSellCountTest.java
@@ -4,15 +4,14 @@ import com.muhuang.salecrawler.sale.Sale;
 import com.muhuang.salecrawler.sale.SaleRepository;
 import com.muhuang.salecrawler.share.TestUtil;
 import jakarta.annotation.Resource;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 
@@ -25,7 +24,7 @@ public class ItemSellCountTest {
     @Nested
     class SingleRealTimeFetch {
 
-        private final String toFetchItemId = "32838242344";
+        private final String itemId = "32838242344";
         private final Date now = new Date();
         String mockJson = TestUtil.readJsonFromResources("taobao_item_get_app_response.json");
 
@@ -43,47 +42,70 @@ public class ItemSellCountTest {
 
         @BeforeEach
         public void setup() {
-            itemRepository.save(TestUtil.createValidItem());
-            Mockito.when(oneBoundService.getTaobaoDetail(toFetchItemId)).thenReturn(mockJson);
-        }
-
-        @AfterEach
-        public void cleanup() {
+            Mockito.when(oneBoundService.getTaobaoDetail(itemId)).thenReturn(mockJson);
             itemRepository.deleteAll();
             saleRepository.deleteAll();
         }
 
         @Test
-        public void toFetchItemId_callSellCountApi_receiveTotalSellCount() {
-            Integer totalSellCount = itemService.getTotalSellCountByOneBound(toFetchItemId);
+        public void fetchItemDetail_callSellCountApi_receiveItemDetailJson() {
+            assertThat(oneBoundService.getTaobaoDetail(itemId)).isNotNull();
+        }
+
+        @Test
+        public void fetchItemDetail_callSellCountApiAndParseJsonToGetSale_receiveSellCount() {
+            Integer totalSellCount = itemService.getTotalSellCountByOneBound(itemId);
             assertThat(totalSellCount).isEqualTo(16);
         }
 
         @Test
-        public void toFetchItemId_callSellCountApi_saveTotalSellCountToDB() {
-            Sale sale = getCurrentDaySale();
-            assertThat(sale.getNumber()).isEqualTo(16);
+        public void fetchItemDetail_callSellCountApiAndParseJsonToGetSale_sellCountSaveToDatabase() {
+            Integer totalSellCount = itemService.getTotalSellCountByOneBound(itemId);
+            itemService.saveSellCount(totalSellCount, itemId, now);
+            assertThat(saleRepository.findAll().size()).isEqualTo(1);
         }
 
         @Test
-        public void toFetchItemId_callSellCountApi_sellCountIsAssociatedItem() {
-            Sale sale = getCurrentDaySale();
-            assertThat(sale.getItem().getOutItemId()).isEqualTo(toFetchItemId);
+        public void saveSellCount_ItemIsValid_sellCountAssociatedWithItem() throws ParseException {
+            itemRepository.save(TestUtil.createValidItem());
+            Integer sellCount = itemService.getTotalSellCountByOneBound(itemId);
+            itemService.saveSellCount(sellCount, itemId);
+            Sale sale = saleRepository.findAll().get(0);
+            assertThat(sale.getItem().getOutItemId()).isEqualTo(itemId);
         }
 
         @Test
-        public void toFetchItemId_callSellCountApi_saveInterDaySellCountToDB() {
-            Date yesterday = Date.from(now.toInstant().minus(Duration.ofDays(1)));
-            itemService.saveSellCount(10, toFetchItemId, yesterday);
-            Sale sale = getCurrentDaySale();
+        @Disabled
+        public void saveSellCount_ItemIsValid_sellCountDateIsYesterday() throws ParseException {
+            Date yesterday = Date.from(new Date().toInstant().minus(Duration.ofDays(1)));
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            String format = simpleDateFormat.format(yesterday);
+            Integer sellCount = itemService.getTotalSellCountByOneBound(itemId);
+            itemService.saveSellCount(sellCount, itemId);
+            Sale sale = saleRepository.findAll().get(0);
+            assertThat(sale.getSaleDate()).isEqualTo(simpleDateFormat.parse(format));
+        }
 
+        @Test
+        public void toFetchItemId_callSellCountApi_saveInterDaySellCountToDB() throws ParseException {
+            Date dayBeforeYesterday = Date.from(now.toInstant().minus(Duration.ofDays(2)));
+            itemService.saveSellCount(10, itemId, dayBeforeYesterday);
+
+            Integer totalSellCount = itemService.getTotalSellCountByOneBound(itemId);
+            itemService.saveSellCount(totalSellCount, itemId);
+            Sale sale = saleRepository.findAll().get(0);
+
+            System.out.println(saleRepository.findAll());
             assertThat(sale.getInterdaySellCount()).isEqualTo(6);
         }
 
+        //第一天存sellCount时，sellCount和当天销量一样
+        //如果sale没有关联item，应该抛出异常
+
         private Sale getCurrentDaySale() {
-            Integer totalSellCount = itemService.getTotalSellCountByOneBound(toFetchItemId);
-            itemService.saveSellCount(totalSellCount, toFetchItemId, now);
-            return itemService.getSale(toFetchItemId, now);
+            Integer totalSellCount = itemService.getTotalSellCountByOneBound(itemId);
+            itemService.saveSellCount(totalSellCount, itemId, now);
+            return itemService.getSale(itemId, now);
         }
     }
 }
