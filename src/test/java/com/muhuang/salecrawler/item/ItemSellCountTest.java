9531fd3d9b8a20f6e45a59fbfc1afc0be4416eb0
kkutopiaa <kk1046@outlook.com>
2024-07-18 11:26:38 +0800

    fix: 修改判断类型，item id 唯一键约束。 feat: SingleRealTimeFetch 添加 sad path

--- src/test/java/com/muhuang/salecrawler/item/ItemSellCountTest.java
+++ src/test/java/com/muhuang/salecrawler/item/ItemSellCountTest.java
@@ -8,14 +8,17 @@ import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -42,9 +45,10 @@ public class ItemSellCountTest {
 
         @BeforeEach
         public void setup() {
-            Mockito.when(oneBoundService.getTaobaoDetail(itemId)).thenReturn(mockJson);
             itemRepository.deleteAll();
             saleRepository.deleteAll();
+            Mockito.when(oneBoundService.getTaobaoDetail(itemId)).thenReturn(mockJson);
+            itemRepository.save(TestUtil.createValidItem());
         }
 
         @Test
@@ -67,7 +71,6 @@ public class ItemSellCountTest {
 
         @Test
         public void saveSellCount_ItemIsValid_sellCountAssociatedWithItem() throws ParseException {
-            itemRepository.save(TestUtil.createValidItem());
             Integer sellCount = itemService.getTotalSellCountByOneBound(itemId);
             itemService.saveSellCount(sellCount, itemId);
             Sale sale = saleRepository.findAll().get(0);
@@ -75,15 +78,22 @@ public class ItemSellCountTest {
         }
 
         @Test
-        @Disabled
+        public void saveSellCount_ItemIsValid_sellCountDateTypeIsJavaSqlTimestamp() throws ParseException {
+            itemService.saveSellCount(1, itemId);
+            Sale sale = saleRepository.findAll().get(0);
+            assertThat(sale.getSaleDate().getClass()).isEqualTo(Timestamp.class);
+        }
+
+        @Test
         public void saveSellCount_ItemIsValid_sellCountDateIsYesterday() throws ParseException {
             Date yesterday = Date.from(new Date().toInstant().minus(Duration.ofDays(1)));
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
             String format = simpleDateFormat.format(yesterday);
+            Timestamp formatedYesterday = new Timestamp(simpleDateFormat.parse(format).getTime());
             Integer sellCount = itemService.getTotalSellCountByOneBound(itemId);
             itemService.saveSellCount(sellCount, itemId);
             Sale sale = saleRepository.findAll().get(0);
-            assertThat(sale.getSaleDate()).isEqualTo(simpleDateFormat.parse(format));
+            assertThat(sale.getSaleDate()).isEqualTo(formatedYesterday);
         }
 
         @Test
@@ -101,6 +111,26 @@ public class ItemSellCountTest {
 
         //第一天存sellCount时，sellCount和当天销量一样
         //如果sale没有关联item，应该抛出异常
+        @Test
+        public void saveSellCount_ItemIsNull_throwSaleAssociatedItemMustNotBeNullException() {
+            itemRepository.deleteAll();
+            assertThatThrownBy(() -> itemService.saveSellCount(1, itemId))
+                    .isInstanceOf(SaleAssociatedItemMustNotBeNullException.class);
+        }
+
+        @Test
+        public void saveSellCount_throwSaleAssociatedItemMustNotBeNullException_hasItemIdMessage() {
+            itemRepository.deleteAll();
+            assertThatThrownBy(() -> itemService.saveSellCount(1, itemId))
+                    .hasMessage("找不到 itemId=32838242344 的商品！");
+        }
+
+        @Test
+        public void saveItem_ItemIdIsDuplicate_throwDataIntegrityViolationException() {
+            assertThatThrownBy(() -> itemRepository.save(TestUtil.createValidItem()))
+                    .isInstanceOf(DataIntegrityViolationException.class);
+        }
+
 
         private Sale getCurrentDaySale() {
             Integer totalSellCount = itemService.getTotalSellCountByOneBound(itemId);
